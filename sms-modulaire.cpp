f0a453fab57e81b48e02bf850521392b753d6f2d
levraidieu <51849937+levraidieu@users.noreply.github.com>
2019-09-30 23:50:56 +0200

    Update sms-modulaire.cpp

--- sms-modulaire.cpp
+++ sms-modulaire.cpp
@@ -3,6 +3,18 @@
 #include <WiFiClientSecure.h>   // connexion https
 #include <Arduino.h>
 
+int boot(){
+	string sms ="boot ou reboot"
+	sms.replace(" ", "%20"); // encode URL espace " " par "%20"
+  	sms.replace("\n", "%0D%0A"); // encode URL a la ligne "\n" par "%0D%0A"
+    envoiSms(sms);
+
+}
+
+
+
+
+
 int smsModulaire1()
   {
 
